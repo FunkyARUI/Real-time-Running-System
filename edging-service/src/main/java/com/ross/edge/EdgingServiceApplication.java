0672d5a5e3cac25c07fcab704be45fd0bf0808ae
FunkyARUI <arui35@outlook.com>
2017-08-11 22:39:47 -0500

    Upload the project

---
+++ edging-service/src/main/java/com/ross/edge/EdgingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ross.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EdgingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgingServiceApplication.class, args);
+    }
+
+}
